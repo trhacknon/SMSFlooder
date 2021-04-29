ccce0bcdd6f34cdaa1a235132fd79f9af2120525
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2021-04-29 10:03:40 +0100

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,8 +1,11 @@
 if ! which python > /dev/null; then
-   echo -e "pythn not found! Installing"
+   echo -e "[!] python not found! Installing"
+   sleep 2
    pkg  install python
    python3 install.py
    
 else
+   echo -e "[+] python already installed installing script req"
+   sleep 2
    python3 install.py
 fi
